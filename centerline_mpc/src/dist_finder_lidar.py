ed5c647559a808d992fb13303cbd51f982f6bd81
Alexandros Filotheou <alefil@kth.se>
2016-11-17 02:44:24 +0100

    Modifies centerline_pid so as to include signs on angles
    Also introduces centerline_mpc package

--- centerline_mpc/src/dist_finder_lidar.py
+++ centerline_mpc/src/dist_finder_lidar.py
@@ -19,7 +19,7 @@ pub = rospy.Publisher('error_topic', input_pid, queue_size=10)
 
 #-------------------------------------------------------------------------------
 #   Input:  data: Lidar scan data
-#           beam_index: The index of the angle at which the distance is required
+#           beam_index: The index of the angle at which the distance is requried
 
 #   OUTPUT: distance of scan at angle theta whose index is beam_index
 #-------------------------------------------------------------------------------
@@ -94,37 +94,6 @@ def getLateralRanges(data):
 
 
 #-------------------------------------------------------------------------------
-# Input:
-#   data: Lidar scan data
-#   beam_index: the beam index corresponding to the angle at which the range
-#               will be found
-#   length: The number of beam_indices whose corresponding range will be
-#           taken
-#
-# Output: Depending on if the length is odd or even, a number of beams are taken
-#         around beam_index in an anti-clockwise fashion. The difference
-#         between consecutive scans is taken and store in list `distances`.
-#         The sum of these differences determines the vehicle's orientation
-#         with respect to the lane that we want it to track.
-#         If the sign of the returned number is positive, that means that the
-#         vehicle is facing the right boundary of the lane. If negative, the
-#         left one.
-#-------------------------------------------------------------------------------
-def getRangeSequenceSign(data, beam_index, length):
-
-    if length % 2 == 1:
-        length = length + 1
-
-    distances = []
-
-    for i in range(-length/2, length/2 + 1)
-        distances.append(getRange(data, beam_index + i + 1) - getRange(data, beam_index + i))
-
-    return sum(distance)
-
-
-
-#-------------------------------------------------------------------------------
 # callback
 #-------------------------------------------------------------------------------
 def callback(data):
@@ -143,40 +112,15 @@ def callback(data):
     F = ranges_list(1)
     L = ranges_list(2)
 
-
-    # Fire auxiliary scans around the front facing one.
-    # This will facilitate the discovery of the vehicle's orientation with
-    # respect to the lane. Take rays across +-{1,2,3,4,5} degrees around F
-    # __anti-clockwise__.
-    # If the sequence is decreasing then the vehicle is facing the left wall.
-    # If the sequence is increasing then the vehicle is facing the right wall.
-
-
     # The overall angular error is: see
-    # https://gits-15.sys.kth.se/alefil/HT16_P2_EL2425_resources/blob/master/Progress%20reports/2016.11.16/main.pdf
+    # https://gits-15.sys.kth.se/alefil/HT16_P2_EL2425_resources/blob/master/various/Progress%20reports/Agendas/2016.11.16/main.pdf
     # The scaling factor R+L is there to make the range disparity invariant to
     # the width of the lane
 
     CCp = 5
     tan_arg_1 = float(L-R) / ((2 * CCp) * (L+R))
-
-    # Take a range scan sequence starting from 1 degree to the right of the main
-    # beam at 0 degrees with respect to the longitudinal axis of the vehicle,
-    # and ending at one degree to the left of it. If the number returned is
-    # positive then the vehicle is facing the right lane boundary. If not,
-    # it's facing the left.
-    if getRangeSequenceSign(data, 540, 8) > 0:
-        tan_arg_2 = -float(R) / F
-    else:
-        tan_arg_2 = float(L) / F
-
-    # If the steering angle to the left requires a negative sign and the
-    # steering angle to the right requires a positive sign, then the vehicle
-    # should turn left if it is facing the right wall and right if it is
-    # facing the left wall. If the vehicle lies at the leftmost half of the
-    # lane, then it should turn right, and if it lies at the rightmost half,
-    # it should turn left.
-    error =  -np.arctan(tan_arg_1) + np.arctan(tan_arg_2)
+    tan_arg_2 = float(F) / R
+    error =  -(np.arctan(tan_arg_1) + np.pi/2 - np.arctan(tan_arg_2))
 
     # Check for angular overflow
     while error > np.pi:
